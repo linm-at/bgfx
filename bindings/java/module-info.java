f59ba6bcd8a584d426ad72500a3ce6c885a4ee49
Markus Lindner <lindner.markus@outlook.at>
2026-08-31 00:36:29 +0200

    Java Bindings: Add module-info

---
+++ bindings/java/module-info.java
@@ -0,0 +1,14 @@
+// Copyright 2011-2026 Branimir Karadzic. All rights reserved.
+// License: https://github.com/bkaradzic/bgfx/blob/master/LICENSE
+
+/**
+ * Java Foreign Function and Memory API bindings for bgfx.
+ */
+module io.github.bkaradzic.bgfx {
+	requires static transitive org.jspecify;
+
+	exports io.github.bkaradzic.bgfx;
+	exports io.github.bkaradzic.bgfx.caps;
+	exports io.github.bkaradzic.bgfx.init;
+	exports io.github.bkaradzic.bgfx.util;
+}
